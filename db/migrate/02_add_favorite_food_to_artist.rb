1861ee1aeb49e53100fc3edda471acaa33932e13
Alice Freda <alicefreda@gmail.com>
2019-12-27 21:59:26 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artist.rb
+++
@@ -1,7 +0,0 @@
-# db/migrate/02_add_favorite_food_to_artists.rb
-
-class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
-  def change
-    add_column :artists, :favorite_food, :string
-  end
-ends
